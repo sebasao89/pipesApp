59e8746a38ad63a8ff8dd8ccbdafde387b874b79
Sebasao <sebas.ao@outlook.com>
2023-07-02 17:17:07 -0500

    uso de pipes UpperCase, LowerCase y TitleCase

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BasicsPageComponent {
 
+  public nameLower: string = 'SEBASTIAN'
+  public nameUpper: string = 'alarcon'
+  public fullName: string = 'sEBAStian aLARcon'
+
 }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -18,15 +18,18 @@ export class MenuComponent implements OnInit{
             items: [
               {
                 label: 'Textos y fechas',
-                icon: 'pi pi-align-left'
+                icon: 'pi pi-align-left',
+                routerLink: '/'
               },
               {
                 label: 'Números',
-                icon: 'pi pi-dollar'
+                icon: 'pi pi-dollar',
+                routerLink: 'numbers'
               },
               {
                 label: 'No comunes',
-                icon: 'pi pi-globe'
+                icon: 'pi pi-globe',
+                routerLink: 'uncommon'
               }
             ]
         },
